c8a6497073899c25bb0334e9568127945109459d
Guilherme Medeiros <guicm007@gmail.com>
2021-10-26 11:20:15 -0300

    feat: add about screen and cleaner function

--- main.c
+++ main.c
@@ -1,6 +1,55 @@
+///////////////////////////////////////////////////////////////////////////////
+///               Universidade Federal do Rio Grande do Norte               ///
+///                   Centro de Ensino Superior do Seridó                   ///
+///                    Discipline DCT1106 -- Programação                    ///
+///                 SIG-Staff -- A system for staff control                 ///
+///           Developed by @guilhermecostam and @victormedeiros1            ///
+///////////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    printf("Welcome to SIG-Staff!");
+    aboutScreen();
     return 0;
+}
+
+/////
+// Functions
+
+/**
+ * @author Guilherme Medeiros
+ * Method that clean the terminal
+ */
+void terminalCleaner(void){
+    printf("\t\t\t>>> Press <ENTER> to continue...\n");
+    getchar();
+    system("clear||cls");
+}
+
+/**
+ * @author Guilherme Medeiros
+ * Method that shows the screen about the project
+ */
+void aboutScreen(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///               Universidade Federal do Rio Grande do Norte               ///\n");
+    printf("///                   Centro de Ensino Superior do Seridó                   ///\n");
+    printf("///                    Discipline DCT1106 -- Programação                    ///\n");
+    printf("///                 SIG-Staff -- A system for staff control                 ///\n");
+    printf("///           Developed by @guilhermecostam and @victormedeiros1            ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                      = = = = = SIG-Staff = = = = =                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///                       A system for staff control                        ///\n");
+    printf("///               Project developed for the discipline DCT1106              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+
+    terminalCleaner();
 }
