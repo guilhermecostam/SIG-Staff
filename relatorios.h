4b05fa5b92b964aa20cf82d3588f17fa66557f85
victormedeiros1 <josevqczt@gmail.com>
2022-02-16 16:33:09 -0300

    feat: relatios.c signature

---
+++ relatorios.h
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+///               Universidade Federal do Rio Grande do Norte               ///
+///                   Centro de Ensino Superior do Seridó                   ///
+///                    Discipline DCT1106 -- Programação                    ///
+///                 SIG-Staff -- A system for staff control                 ///
+///           Developed by @guilhermecostam and @victormedeiros1            ///
+///////////////////////////////////////////////////////////////////////////////
+
+void listStaffOptionsScreen(void);
+void listStaff(void);
+void listStaffAlphabetic(void);
